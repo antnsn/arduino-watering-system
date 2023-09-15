43531b38914cb3ab50491cbf368a9d7c954e5529
Marius Antonsen <antonsen.marius@gmail.com>
2023-09-16 01:31:07 +0200

    Update arduino-watering-system_oop.ino

--- arduino-watering-system_oop/arduino-watering-system_oop.ino
+++ arduino-watering-system_oop/arduino-watering-system_oop.ino
@@ -1,8 +1,10 @@
+// Define a class for a plant
 class Plant {
-  int sensorPin;
-  int pumpPin;
+  int sensorPin;  // Pin connected to the plant's sensor
+  int pumpPin;    // Pin connected to the plant's pump
 
 public:
+  // Constructor to initialize pins
   Plant(int sensPin, int pPin) {
     sensorPin = sensPin;
     pinMode(sensorPin, INPUT);
@@ -10,18 +12,19 @@ public:
     pinMode(pumpPin, OUTPUT);
   }
 
+  // Method to water the plant based on sensor status
   void waterPlant() {
     if (digitalRead(sensorPin) == LOW) {
-      digitalWrite(pumpPin, HIGH);
+      digitalWrite(pumpPin, HIGH);  // Activate pump if sensor is LOW
     } else {
-      digitalWrite(pumpPin, LOW);
+      digitalWrite(pumpPin, LOW);   // Deactivate pump if sensor is not LOW
     }
   }
 };
 
-const int numPlants = 8;
+const int numPlants = 8;  // Number of plants
 Plant plants[numPlants] = {
-  Plant(A0, 2),
+  Plant(A0, 2),  // Create instances for each plant with sensor and pump pins
   Plant(A1, 3),
   Plant(A2, 4),
   Plant(A3, 5),
@@ -32,10 +35,11 @@ Plant plants[numPlants] = {
 };
 
 void setup() {
-  // Add any setup code if needed
+  // Setup code (if needed)
 }
 
 void loop() {
+  // Loop through all the plants and water them based on sensor status
   for (int i = 0; i < numPlants; i++) {
     plants[i].waterPlant();
   }
